4c8b5f2e4f82140d7243f97609dded46ac171ffd
kbaitubayev <kbaitubayev@myseneca.ca>
2024-02-19 18:22:33 -0500

    services page added

--- pages/services.js
+++ pages/services.js
@@ -1,73 +1,65 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
-import image1 from "../assests/image1.jpg";
+// pages/services.js
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
 
-export default function Services() {
+const Services = () => {
+  const [servicesData, setServicesData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from the server when the component mounts
+    const fetchData = async () => {
+      try {
+        const response = await api.get('/services');
+        setServicesData(response.data.services);
+      } catch (error) {
+        console.error('Error fetching services data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <Row className={`${styles["row-style"]}`}>
-        <Col>
-          <Image
-            src={image1}
-            alt="Gallery image"
-            className={`${styles["service-image"]}`}
-          />
-        </Col>
-        <Col className={`${styles["service-description"]}`}>
-          <h2>Deluxe Bath Package</h2>
-          <ul>
-            <li>Shampoo</li>
-            <li>Conditioner</li>
-            <li>Blow Dry</li>
-            <li>Brush</li>
-            <li>Ear Cleaning</li>
-            <li>Nail Trim</li>
-          </ul>
-        </Col>
-      </Row>
+      <h1>{servicesData?.title}</h1>
+      <p>{servicesData?.description}</p>
 
-      <Row className={`${styles["row-style"]}`}>
-        <Col>
-          <Image
-            src={image1}
-            alt="Gallery image"
-            className={`${styles["service-image"]}`}
-          />
-        </Col>
-        <Col className={`${styles["service-description"]}`}>
-          <h2>Full Package</h2>
-          <ul>
-            <li>Shampoo</li>
-            <li>Conditioner</li>
-            <li>Blow Dry</li>
-            <li>Brush</li>
-            <li>Ear Cleaning</li>
-            <li>Nail Trim</li>
-            <li>Hair clipping</li>
-          </ul>
-        </Col>
-      </Row>
-
-      <Row className={`${styles["row-style"]}`}>
-        <Col>
-          <Image
-            src={image1}
-            alt="Gallery image"
-            className={`${styles["service-image"]}`}
-          />
-        </Col>
-        <Col className={`${styles["service-description"]}`}>
-          <h2>Shedless Treatment</h2>
-          <ul>
-            <li>Full Package with the Shedless Treatment</li>
-            <li>Bath with de-shedding solution</li>
-            <li>Deep brushing to extract loose dead hair</li>
-
-          </ul>
-        </Col>
-      </Row>
+      <div className="row justify-content-center">
+        {servicesData?.servicesList.map((service, index) => (
+          <div key={index} className="col-md-4 offset-md-0 mb-4">
+            {/* Adjust the column size based on your preference */}
+            <div className="card">
+              <div className="row g-0">
+                {/* First row, left column (contains picture) */}
+                <div className="col-md-3">
+                <img
+    src={service.imageUrl}
+    alt={`${service.title} Image`}
+    style={{ maxWidth: "100px" }} // Set the desired maximum width
+    className="card-img-top"
+  />
+                </div>
+                {/* First row, right column (contains card title) */}
+                <div className="col-md-9 d-flex align-items-center">
+  <div className="card-body">
+    <h5 className="card-title">{service.title}</h5>
+  </div>
+</div>
+              </div>
+              {/* Second row (contains text with description) */}
+              <div className="row">
+  <div className="col-md-12">
+    <div className="card-body">
+      <p className="card-text" dangerouslySetInnerHTML={{ __html: service.text.replace(/\n/g, '<br/>') }}></p>
+    </div>
+  </div>
+</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+export default Services;
