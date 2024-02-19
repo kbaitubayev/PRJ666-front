3d6a473b4bb89713d183e5642573325c385744a1
kbaitubayev <kbaitubayev@myseneca.ca>
2024-02-18 23:27:43 -0500

    about.js added

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,37 @@
-import React from 'react'
+// pages/about.js
 
-export default function about () {
+import React, { useState, useEffect } from 'react';
+
+const AboutPage = () => {
+  const [aboutData, setAboutData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/about');
+        const data = await response.json();
+        setAboutData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>about</div>
-  )
-}
+    <div>
+      <h2>Page about us !</h2>
+      {aboutData ? (
+        <div>
+          {/* Render your about page content using aboutData */}
+          <p>{aboutData.about}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default AboutPage;
